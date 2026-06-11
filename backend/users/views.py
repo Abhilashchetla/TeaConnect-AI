8ea4ba609954eefb1772a6ddcf97d5e920a10937
unknown <chabhilash5@gmail.com>
2026-06-11 17:49:30 +0530

    Day 3 - JWT authentication setup

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
 
-# Create your views here.
+@api_view(['POST'])
+def register_user(request):
+
+    user = User.objects.create(
+        name=request.data.get('name'),
+        email=request.data.get('email'),
+        phone=request.data.get('phone'),
+        role=request.data.get('role')
+    )
+
+    return Response({
+        "message": "User Registered Successfully"
+    })
